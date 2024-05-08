5dcda7ad8fc8042887b64422227413a4cf2e124c
Muhammad Mukhlis <107677479+MuhammadMukhlis220@users.noreply.github.com>
2024-05-08 11:47:35 +0700

    Update test_connection_postgre_2.py

--- test_connection_postgre_2.py
+++ test_connection_postgre_2.py
@@ -68,6 +68,7 @@ st.plotly_chart(fig)
 
 
 df3 = df3['category_name'].value_counts().reset_index().sort_values('count')
+df3.columns = ['title', 'category_name']
 # df_reversed = df3[::-1]
 fig = go.Figure(go.Bar(
     x=df3['count'],
